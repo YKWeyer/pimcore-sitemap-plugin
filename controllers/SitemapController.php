f95c413cf44db50f017081b091962ece84dc3b1d
Yann Weyer <yann.weyer@orderbird.com>
2016-09-01 15:27:33 +0200

    Update documentation and formatting

--- controllers/SitemapController.php
+++ controllers/SitemapController.php
@@ -1,9 +1,16 @@
 <?php
 
 /**
- * Pimcoresitemapplugin_SitemapController
+ * This file is part of the pimcore-sitemap-plugin package.
  *
- * @author Yann Weyer
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 use Byng\Pimcore\Sitemap\SitemapPlugin as SitemapPlugin;
@@ -12,8 +19,10 @@ use Pimcore\Controller\Action\Frontend as Frontend;
 final class Pimcoresitemapplugin_SitemapController extends Frontend
 {
 
-    /*
+    /**
      * Shows the sitemap.xml of the currently visited website
+     *
+     * @return void
      */
     public function viewAction()
     {
@@ -22,7 +31,7 @@ final class Pimcoresitemapplugin_SitemapController extends Frontend
             $filename = '/' . $site->getMainDomain() . '.xml';
         } else {
             // Default filename
-            $filename = '/'. \Pimcore\Config::getSystemConfig()->get("general")->get("domain") . '.xml';
+            $filename = '/' . \Pimcore\Config::getSystemConfig()->get("general")->get("domain") . '.xml';
         }
 
         // Outputting the XML
